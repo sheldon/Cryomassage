1b6afb373116225fa81d621ed7c3e21cbf009a75
Sheldon <sheldon.els@gmail.com>
2010-10-11 17:10:17 +0100

    holding page

--- app/controller/PageController.php
+++ app/controller/PageController.php
@@ -1,5 +1,9 @@
 <?php
 class PageController extends ApplicationController{
+  public function controller_global(){
+    $this->use_layout = "holding";
+    $this->use_view = false;
+  }
   public function index(){}
   public function _top_nav(){
     $this->sects = new CmsSection;
