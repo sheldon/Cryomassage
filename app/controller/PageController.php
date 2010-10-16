10e831d1ba92cd6ebae292206deecaa03082f13a
Sheldon <sheldon.els@gmail.com>
2010-10-16 20:20:08 +0100

    booking form

--- app/controller/PageController.php
+++ app/controller/PageController.php
@@ -10,6 +10,20 @@ class PageController extends ApplicationController{
     }
     $this->sects = new CmsSection;
     $this->sects = $this->sects->roots();
+
+    $this->bookingform = new WaxForm;
+    $this->bookingform->add_element("name", "TextInput");
+    $this->bookingform->add_element("email_address", "TextInput");
+    
+    if(($this->results = $this->bookingform->save()) && !$_POST['dontfillthisin']){
+      $email = new WaxEmail;
+      $email->add_to_address("sheldon.els@gmail.com", "sheldon");
+      $email->add_bcc_address("pawel@cryomassage.co.uk", "pawel");
+      $email->subject = "New booking from website";
+      $email->body = "Details from the form:\n";
+      foreach ($this->results as $key => $val) $email->body .= "\n$key : $val";
+      $email->send();
+    }
   }
   public function index(){}
 }
