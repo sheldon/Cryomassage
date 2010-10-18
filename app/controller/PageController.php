77eabf541c9d914025f0d34b1f7b7e29fe62f2f3
Sheldon <sheldon.els@gmail.com>
2010-10-18 22:02:48 +0100

    holding page gone

--- app/controller/PageController.php
+++ app/controller/PageController.php
@@ -1,13 +1,8 @@
 <?php
 class PageController extends ApplicationController{
   public function controller_global(){
-    if(($normal_layout = Request::param("test")) || ($normal_layout = Session::get("normal_layout"))){
-      Session::set("normal_layout", $normal_layout);
-      $this->cms();
-    }else{
-      $this->use_layout = "holding";
-      $this->use_view = false;
-    }
+    $this->cms();
+
     $this->sects = new CmsSection;
     $this->sects = $this->sects->roots();
 
